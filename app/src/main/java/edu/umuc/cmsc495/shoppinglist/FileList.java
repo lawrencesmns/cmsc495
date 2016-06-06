8e38e98b23b2a50e5c59821e9357702d34f17309
James <jalexbarbour@gmail.com>
2016-06-06 19:19:16 -0400

    Adding FileLists() class.

---
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/FileList.java
@@ -0,0 +1,31 @@
+package edu.umuc.cmsc495.shoppinglist;
+
+import java.util.*;
+
+/**
+ * Created by Alex on 6/6/16.
+ * FileList is the backend's interface to the GUI.
+ */
+public class FileList {
+
+    //Global Variables
+    protected List<FileListItem> lists;
+
+    /**
+     * getLists() provides the method to call from the FileList object in order
+     * access the shopping list files.
+     */
+    protected List<FileListItem> getLists(){
+
+        //TODO: Change return type once method is built.
+        return null;
+    }
+
+    /**
+     * populateList() provides the function of each list to populate into a
+     * list object for the program
+     */
+    private void populateList(){
+
+    }
+}
