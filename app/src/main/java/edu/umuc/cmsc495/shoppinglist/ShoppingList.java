e3718242632f26bc994d9620ee7b74788a7f5839
James <jalexbarbour@gmail.com>
2016-06-07 10:57:02 -0400

    Pushing ShoppingList().

--- app/src/main/java/edu/umuc/cmsc495/shoppinglist/ShoppingList.java
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/ShoppingList.java
@@ -12,6 +12,7 @@ public class ShoppingList {
     //Class Variables
     String name;
     List<Ingredient> Ingredients;
+    String emailBody, emailSubject;
 
     /**
      * Constructor for the shopping list. Fills the name and list objects.
@@ -21,6 +22,77 @@ public class ShoppingList {
         this.name = name;
         Ingredients = new ArrayList<Ingredient>();
     }
+
+
+    //Methods to add items to the list from a given recipe
+
+    /**
+     * Iterates through the given recipe's ingredients and checks if the ingredient is already present.
+     * If it is, it updates the quantity on the shopping list's ingredient.
+     * If it is NOT, it simply adds the ingredient to the list.
+     * @param newRecipe
+     * @return true
+     */
+    protected boolean addRecipe(Recipe newRecipe){
+        List<Ingredient> recipeIngredients = newRecipe.getIngredientList();
+
+        for(Ingredient newIngredient: recipeIngredients){
+            if(isIngredientAlreadyPresent(newIngredient)){
+                int indexOf = getIndexOfName(newIngredient);
+                Ingredient targetIngredient = this.Ingredients.get(indexOf);
+                targetIngredient = updateIngredient(targetIngredient, newIngredient);
+                this.Ingredients.set(indexOf, targetIngredient);
+            }else{
+                addIngredient(newIngredient);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * updates the quantities of oldIngredient by adding the quantities of newIngredient.
+     * @param oldIngredient
+     * @param newIngredient
+     * @return updated Ingredient
+     */
+    private Ingredient updateIngredient(Ingredient oldIngredient, Ingredient newIngredient){
+        oldIngredient.addCount(newIngredient.getCount());
+        oldIngredient.addMeasurement(newIngredient.getMeasurement());
+        return oldIngredient;
+    }
+
+    /**
+     * Checks if the given ingredient is present in the list by comparing the names
+     * @param ingredient
+     * @return
+     */
+    private boolean isIngredientAlreadyPresent(Ingredient ingredient){
+        for(Ingredient currIngredient : Ingredients){
+            if(currIngredient.getName().compareTo(ingredient.getName()) == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Attempts to locate the index of the ingredient name provided.
+     * If found, returns the index of the matching ingredient.
+     * Return -1 if no match if found.
+     * @param targetIngredient
+     * @return integer index of ingredient
+     */
+    private int getIndexOfName(Ingredient targetIngredient){
+        for(Ingredient ingredient: Ingredients){
+            if(ingredient.getName().compareTo(targetIngredient.getName()) == 0){
+                return Ingredients.indexOf(ingredient);
+            }
+        }
+        return -1;
+    }
+
+
+    //List Operations
 
     /**
      * Attempts to add an ingredient object to the list.
@@ -97,5 +169,13 @@ public class ShoppingList {
         //TODO: Implement call to DataLayer to erase file.
 
         return checkFlag;
+    }
+
+    //Get Methods
+    protected String getEmailBody(){
+        return this.emailBody;
+    }
+    protected String getEmailSubject(){
+        return this.emailSubject;
     }
 }
