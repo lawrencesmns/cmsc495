a4f79bedd7044f6b1df4d3826c13a679a2569637
highpwner <jalexbarbour@gmail.com>
2016-06-06 23:42:12 -0400

    Updating FileListItem()

--- app/src/main/java/edu/umuc/cmsc495/shoppinglist/FileListItem.java
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/FileListItem.java
@@ -1,7 +1,13 @@
 package edu.umuc.cmsc495.shoppinglist;
 
+import java.util.*;
 /**
  * Created by James on 6/6/2016.
  */
 public class FileListItem{
+
+    String name;
+    Date createdOn, LastModifiedOn;
+
+
 }
