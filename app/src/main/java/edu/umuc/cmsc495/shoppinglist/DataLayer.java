3a84e502c28ddb56aaefad26fe1528598bf7137e
James <jalexbarbour@gmail.com>
2016-06-07 11:31:51 -0400

    Adding what is done on DataLayer yet so far.

---
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/DataLayer.java
@@ -0,0 +1,58 @@
+package edu.umuc.cmsc495.shoppinglist;
+
+import android.nfc.Tag;
+import android.util.Log;
+
+import java.io.*;
+
+/**
+ * Created by James on 6/7/2016.
+ * Last Modified by james on 6/7/2016.
+ */
+public class DataLayer {
+
+    //Class Variables
+    String RECIPE_FILE_PREFIX = "r_";
+    String SHOPPINGLIST_FILE_PREFIX = "sl_";
+    char[] INVALID_FILE_NAME_CHARS = {'?',':','\\','\"','\'','*','|','/','<','>',';'};
+    FileOutputStream outputStream;
+
+    public void main(String args[]){
+    }
+
+    protected boolean addRecipe(Recipe newRecipe){
+
+        return true;
+    }
+
+    protected boolean addShoppingList(ShoppingList shoppingList){
+
+        return true;
+    }
+
+    protected boolean deleteRecipe(Recipe targetRecipe){
+
+        return true;
+    }
+
+    protected boolean deleteShoppingList(ShoppingList targetShoppingList){
+
+        return true;
+    }
+
+    protected FileList getRecipeList(){
+
+    }
+
+    protected FileList getShoppingLists(){
+
+    }
+
+    protected Recipe getRecipe(){
+
+    }
+
+    protected ShoppingList getShoppingList(){
+
+    }
+}
