3aae2de79711838772a865c32f720d4f1074782d
Lawrence <lawrencesmns@gmail.com>
2016-06-08 12:29:07 -0400

    Added UPC scanner to MainActivity

--- app/src/main/java/edu/umuc/cmsc495/shoppinglist/MainActivity.java
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/MainActivity.java
@@ -1,7 +1,12 @@
 package edu.umuc.cmsc495.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.umuc.cmsc495.shoppinglist.barcode.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    public void startBarcodeCaptureActivity() {
+        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+        startActivity(intent);
+    }
+
+    public void initialize(){
+        Button btnBarcodeTest = (Button) findViewById(R.id.barcode_test);
+
+        btnBarcodeTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBarcodeCaptureActivity();
+            }
+        });
     }
 }
