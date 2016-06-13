8f2a5dae01377f11e77fdafbb8bafc4b35f9ff46
Lawrence <lawrencesmns@gmail.com>
2016-06-13 19:14:01 -0400

    Adding up navigation to NewShoppingListFragment
    This up navigation isn't exactly per Google's guidelines, but it works for this fragment setup.

--- app/src/main/java/edu/umuc/cmsc495/shoppinglist/MainActivity.java
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,11 +20,12 @@ public class MainActivity extends AppCompatActivity implements NewShoppingListFr
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         initialize();
     }
 
+    /*This method initializes all UI elements for this activity.*/
     void initialize(){
+        setContentView(R.layout.activity_main);
         Button btnNewShoppingList = (Button) findViewById(R.id.btn_new_list);
         btnNewShoppingList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -74,8 +77,25 @@ public class MainActivity extends AppCompatActivity implements NewShoppingListFr
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
+
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                ActionBar actionBar = getSupportActionBar();
+                actionBar.setTitle(R.string.app_name);
+                actionBar.setDisplayHomeAsUpEnabled(false);
+                initialize();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
     @Override
     public void onFragmentInteraction(Uri uri) {
 
