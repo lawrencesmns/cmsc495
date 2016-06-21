fc8cc0a0f6b9099a17f5f6057c0745726599cb5b
Mike Bradshaw <liquid1182@msn.com>
2016-06-20 22:46:35 -0400

    Added activity Added New ingredient Activity

--- app/src/main/java/edu/umuc/cmsc495/shoppinglist/UI/RecipesList.java
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/UI/RecipesList.java
@@ -1,7 +1,9 @@
 package edu.umuc.cmsc495.shoppinglist.UI;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -103,8 +105,11 @@ public class RecipesList extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+               //        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(),NewIngredient.class);
+                startActivity(intent);
+
             }
         });
     }
