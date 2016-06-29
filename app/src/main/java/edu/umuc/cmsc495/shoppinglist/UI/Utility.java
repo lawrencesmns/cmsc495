30ae572fed1c2832869d5b721e8747d8548f0828
Lawrence <lawrencesmns@gmail.com>
2016-06-29 14:51:04 -0400

    Adding Utility class

---
+++ app/src/main/java/edu/umuc/cmsc495/shoppinglist/UI/Utility.java
@@ -0,0 +1,28 @@
+package edu.umuc.cmsc495.shoppinglist.UI;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by lawre on 6/29/2016.
+ */
+public class Utility {
+
+    public static void composeEmail(String subject, String body, Context context) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            context.startActivity(intent);
+        }
+    }
+
+    public static void debugAlert(Context context, String message){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setMessage(message);
+        alertBuilder.create().show();
+    }
+
+}
